40ef6ac6cebc44b3c423ba8dafebaf801f1fa5d6
kunduz14 <49558346+kunduz14@users.noreply.github.com>
2024-12-15 19:30:51 +0400

    KS-3-1-greeting

---
+++ src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_3
+
+fun main() {
+    var user = "Андреева Татьяна Сергеевна"
+    var age = 20
+
+    println("$user, $age")
+
+    user = "Сидорова Татьяна Сергеевна"
+    age = 22
+
+    println("$user, $age")
+}
