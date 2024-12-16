fc111dad22af123cb684d59932e9c7bca0177e7f
kunduz14 <49558346+kunduz14@users.noreply.github.com>
2024-12-16 16:49:33 +0400

    KS-3-5 refactor split

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -2,9 +2,10 @@ package org.example.lesson_3
 
 fun main() {
     val send = "D2-D4;0"
-    val whereFrom = send.split('-', ';') [0]
-    val where = send.split('-', ';') [1]
-    val moveNumber = send.split('-', ';') [2]
+    val sendToList = send.split('-', ';')
+    val whereFrom = sendToList[0]
+    val where = sendToList[1]
+    val moveNumber = sendToList[2]
 
     println(whereFrom)
     println(where)
