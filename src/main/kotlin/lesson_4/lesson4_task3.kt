38e3f9898af3e333ee5902044f49a4a16c3f3a5e
kunduz14 <49558346+kunduz14@users.noreply.github.com>
2024-12-16 22:18:41 +0400

    KS-4-3 refactor

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,17 +1,18 @@
 package org.example.lesson_4
 
-const val SUNNY_WEATHER = true
-const val OPEN_AWNING = true
+const val IS_SUNNY = true
+const val IS_AWNING_OPEN = true
 const val AIR_HUMIDITY = 20
-const val SEASON = "Not winter"
+const val SEASON = "Winter"
 
 fun main() {
-    val weatherTodayIsSunny = true
-    val awningIsOpen = true
+    val isSunny = true
+    val isAwningOpen = true
     val airHumidity = 20
     val currentSeason = "Winter"
 
-    println("Благоприятные ли условия сейчас для роста бобовых? " +
-            "${weatherTodayIsSunny == SUNNY_WEATHER && awningIsOpen == OPEN_AWNING 
-                    && airHumidity == AIR_HUMIDITY && currentSeason == SEASON}")
+    println(
+        "Благоприятные ли условия сейчас для роста бобовых? " +
+                "${isSunny == IS_SUNNY && isAwningOpen == IS_AWNING_OPEN && airHumidity == AIR_HUMIDITY && currentSeason != SEASON}"
+    )
 }
