537fb24729968158214edf65aa780a6702a8b8ad
kunduz14 <49558346+kunduz14@users.noreply.github.com>
2024-12-17 20:50:44 +0400

    KS-4-4 refactor

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -4,21 +4,12 @@ fun main() {
     val day = 5
     val isEvenDay = day % 2 == 0
 
-    if (isEvenDay == true) {
-        println("""
-            Упражнения для рук:    false
-            Упражнения для ног:    true
-            Упражнения для спины:  true
-            Упражнения для пресса: false
+    println(
+        """
+            Упражнения для рук:    ${!isEvenDay}
+            Упражнения для ног:    ${isEvenDay}
+            Упражнения для спины:  ${isEvenDay}
+            Упражнения для пресса: ${!isEvenDay}
         """.trimIndent()
-        )
-    } else {
-        println("""
-            Упражнения для рук:    true
-            Упражнения для ног:    false
-            Упражнения для спины:  false
-            Упражнения для пресса: true
-        """.trimIndent()
-        )
-    }
+    )
 }
